66dca5e2a3a8f0af63cfcf8bd99ce3ec75abd647
vicodin <jazper78@mail.ru>
2022-04-30 18:50:37 +0300

    Добаботка 2

--- Netology_IB_Instruments'/Profile/ProfileHeaderView.swift
+++ Netology_IB_Instruments'/Profile/ProfileHeaderView.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private var statusText: String?
     
     let myView: UIView = {
--- Netology_IB_Instruments'/Profile/ProfileViewController.swift
+++ Netology_IB_Instruments'/Profile/ProfileViewController.swift
@@ -14,7 +14,6 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray6
-        headerView.layout()
         customHeaderView()
     }
     
