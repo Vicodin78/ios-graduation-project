7fa9f5ee689a6f6eb3ddbfc114e9e99b2d3a1b09
vicodin <jazper78@mail.ru>
2022-05-02 01:21:55 +0300

    Поиски alfa backgroundimage

--- Netology_IB_Instruments'/Profile/LogInViewController.swift
+++ Netology_IB_Instruments'/Profile/LogInViewController.swift
@@ -14,13 +14,13 @@ class LogInViewController: UIViewController {
         customLogInView()
     }
     
-    let logoImg: UIImageView = {
+    private let logoImg: UIImageView = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.image = UIImage(named: "logo")
         return $0
     }(UIImageView())
     
-    let stackLogIn: UIStackView = {
+    private let stackLogIn: UIStackView = {
         let stack = UIStackView()
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
@@ -31,7 +31,7 @@ class LogInViewController: UIViewController {
         return stack
     }()
     
-    let loginTextField: UITextField = {
+    private let loginTextField: UITextField = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.placeholder = "Email of phone"
         $0.textColor = .black
@@ -42,12 +42,12 @@ class LogInViewController: UIViewController {
         return $0
     }(UITextField())
     
-    let lineBetweenTextFields: UIView = {
+    private let lineBetweenTextFields: UIView = {
         $0.backgroundColor = .lightGray
         return $0
     }(UIView())
     
-    let passTextField: UITextField = {
+    private let passTextField: UITextField = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.placeholder = "Password"
         $0.textColor = .black
@@ -59,12 +59,24 @@ class LogInViewController: UIViewController {
         return $0
     }(UITextField())
     
+    private let buttonLogIn: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setTitle("Log In", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = UIColor("#4885CC", alpha: 1)
+//        $0.setBackgroundImage(UIImage(named: "blue_pixel"), for: .highlighted)
+//        $0.backgroundImage(for: .disabled)
+//        $0.backgroundImage(for: .selected)
+        $0.layer.cornerRadius = 10
+        return $0
+    }(UIButton())
+    
     private func customLogInView() {
         view.backgroundColor = .white
         
         [loginTextField, lineBetweenTextFields, passTextField].forEach { stackLogIn.addArrangedSubview($0)}
         
-        [logoImg, stackLogIn].forEach { view.addSubview($0) }
+        [logoImg, stackLogIn, buttonLogIn].forEach { view.addSubview($0) }
         
         NSLayoutConstraint.activate([
             logoImg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
@@ -79,8 +91,35 @@ class LogInViewController: UIViewController {
        
             loginTextField.heightAnchor.constraint(equalToConstant: 50),
             passTextField.heightAnchor.constraint(equalToConstant: 50),
-            lineBetweenTextFields.heightAnchor.constraint(equalToConstant: 0.5)
+            lineBetweenTextFields.heightAnchor.constraint(equalToConstant: 0.5),
+            
+            buttonLogIn.topAnchor.constraint(equalTo: stackLogIn.bottomAnchor, constant: 16),
+            buttonLogIn.leadingAnchor.constraint(equalTo: stackLogIn.leadingAnchor),
+            buttonLogIn.trailingAnchor.constraint(equalTo: stackLogIn.trailingAnchor),
+            buttonLogIn.heightAnchor.constraint(equalToConstant: 50)
         ])
-        
     }
+}
+
+extension UIColor {
+  
+  convenience init(_ hex: String, alpha: CGFloat = 1.0) {
+    var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    
+    if cString.hasPrefix("#") { cString.removeFirst() }
+    
+    if cString.count != 6 {
+      self.init("ff0000")
+      return
+    }
+    
+    var rgbValue: UInt64 = 0
+    Scanner(string: cString).scanHexInt64(&rgbValue)
+    
+    self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+              green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+              blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+              alpha: alpha)
+  }
+
 }
